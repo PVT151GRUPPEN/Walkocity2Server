8154d37ed9ffd142d73483662fa4700dffc67a0c
1000ol <hbirgestam@gmail.com>
2021-05-13 14:09:09 +0200

    BadgeKey has constructor

--- src/main/java/com/example/Walkocity/accessingdatamysql/BadgeKey.java
+++ src/main/java/com/example/Walkocity/accessingdatamysql/BadgeKey.java
@@ -5,4 +5,9 @@ import java.io.Serializable;
 public class BadgeKey implements Serializable { //varför serializable
     private Badge.Category category;
     private Badge.Level level;
+
+    public BadgeKey(Badge.Category category, Badge.Level level) {
+        this.category = category;
+        this.level = level;
+    }
 }
