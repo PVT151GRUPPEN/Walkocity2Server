f878410b7f3b7cd7f363b8d0e236eca56b963cfc
Aromn <arfr5255@student.su.se>
2021-05-27 16:41:33 +0200

    klassnamn = tabellnamn (små bokstäver)
    test med localhost
    tillbaks
    test med user
    tillbaks med trashcan
    Double double
    tgo bort alla double i trashcn
    
    allt är bra nu
    
    nä, nu
    
    ChatServer  demokod   ++ index test
    
    points and stations test

--- src/main/java/com/example/Walkocity/accessingdatamysql/UserAccomplishment.java
+++ src/main/java/com/example/Walkocity/accessingdatamysql/UserAccomplishment.java
@@ -8,7 +8,7 @@ public class UserAccomplishment { //detta ska då kopplas ihop med det som ska d
     private static final Integer TOILET_STATION = 100;
     private static final Integer CAFE_STATION = 100;
     private static final String TRASH_STATION_TYPE = "trash";
-    private static final String TOILET_STATION_TYPE = "toilet";
+    private static final String TOILET_STATION_TYPE = "Toalett";
     private static final String CAFE_STATION_TYPE = " cafe";
 
 
